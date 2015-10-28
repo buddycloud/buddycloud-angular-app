54beb50d4b01208f3a9af0e830f0888eb654e81a
Simon Tennant <simon@buddycloud.com>
2015-10-28 15:04:09 +0100

    Create settings.js
    These seem to be the only settings we'd be needing.

---
+++ settings.js
@@ -0,0 +1,14 @@
+define({
+  // Where to find your API server (defaults to the buddycloud dev server - set
+  // to your own server.
+  // Note: Browsers do not permit using self signed certificates for AJAX requests
+  rest-api-endpoint: 'http://laos.buddycloud.com/api',
+  
+  // Domain that you your users live on.
+  // For example, if your users' IDs look like user@example.com, then you would 
+  // set the homeDomain to example.com.
+  xmpp-domain: 'laos.buddycloud.com',
+  
+  // Opengraph endpoint
+  opengraphendpoint: 'https://opengraph-parser.herokuapp.com'
+});
